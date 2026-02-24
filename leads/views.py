6ec7a30b27dec5191a2e7fd1fd74fffc0fba848a
AGWEMIS <majidov.intech@gmail.com>
2026-02-24 16:10:55 +0500

    Added Bootstrap landing page and updated CRM lead templates

--- leads/views.py
+++ leads/views.py
@@ -4,6 +4,10 @@ from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
 
+def landing_page(request):
+    return render(request, "landing_page.html")
+
+
 def leads_list(request):
 
     leads = Lead.objects.all()
